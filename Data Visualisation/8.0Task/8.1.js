d781d6f37e3426ef48b6c735739dd0b9030c885c
Yozzyyy <Yapbryan602@gmail.com>
2024-10-17 12:59:53 +0800

    map

--- Data Visualisation/8.0Task/8.1.js
+++ Data Visualisation/8.0Task/8.1.js
@@ -1,19 +1,22 @@
 function init(){
-
-
+    //Width and height
     var w = 500;
     var h = 300;
-    
-    var projection = d3.geoMercator()
-                        
-    var path = d3.geoPath()
-                 .projection(projection);
 
-    var svg = d3.select("body")
+    var projection = d3.geoMercator()
+                        .center([145, -36.5])
+                        .translate([w / 2, h / 2])
+                        .scale(3000);
+
+    // set up the paths
+    var path = d3.geoPath()
+                .projection(projection);
+
+    var svg = d3.select("#Map")
                 .append("svg")
-                .attr("width", w)
-                .attr("height", h)
-                .attr("fill", "grey");
+                .attr("width",w)
+                .attr("height",h)
+                .attr("fill","steelblue");
 
     d3.json("LGA_VIC.json").then(function(json){
 
@@ -22,6 +25,6 @@ function init(){
             .enter()
             .append("path")
             .attr("d", path);
-
     });
 }
+window.onload = init;
