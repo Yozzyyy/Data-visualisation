18442968181a36626137d87e6a5f6da60f50ba59
Yozzyyy <Yapbryan602@gmail.com>
2024-10-24 00:03:14 +0800

    da

---
+++ Data Visualisation/8.0Task/8.2.js
@@ -0,0 +1,30 @@
+function init(){
+    //Width and height
+    var w = 500;
+    var h = 300;
+
+    var projection = d3.geoMercator()
+                        .center([145, -36.5])
+                        .translate([w / 2, h / 2])
+                        .scale(3000);
+
+    // set up the paths
+    var path = d3.geoPath()
+                .projection(projection);
+
+    var svg = d3.select("#Map")
+                .append("svg")
+                .attr("width",w)
+                .attr("height",h)
+                .attr("fill","steelblue");
+
+    d3.json("https://raw.githubusercontent.com/Yozzyyy/Data-visualisation/main/Data%20Visualisation/8.0Task/LGA_VIC.json").then(function(json){
+
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path);
+    });
+}
+window.onload = init;
